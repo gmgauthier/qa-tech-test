85606eb917203dfac3fe55dd2afc23c1ed376309
Greg Gauthier <greg@gmgauthier.com>
2020-09-29 10:41:59 +0100

    final commit

--- src/test/e2e/run_tests.sh
+++ src/test/e2e/run_tests.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-yarn && yarn start &
+cd ../../..
+docker build -t ecsd-tech-test .
+docker run -d -p 3000:3000 ecsd-tech-test:latest &
+CONTAINER=$(docker ps -lq)
 
+cd src/test/e2e
 PYTHON=$(command -v python3)
 $PYTHON -m pip install -r requirements.txt
-
 pytest -v
-
-yarn stop
+docker stop $CONTAINER
